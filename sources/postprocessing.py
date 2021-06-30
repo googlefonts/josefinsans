1ead51113e34a258fd74ada9e2951dad4becf911
Aaron Bell <aaron@sajatypeworks.com>
2021-06-29 21:04:36 -0700

    working...

--- sources/postprocessing.py
+++ sources/postprocessing.py
@@ -11,14 +11,14 @@ for path in font_paths:
     font = TTFont(path)
     # font["name"].setName("Josefin Sans", 1, 3, 1, 1033)
     # if "Italic" in str(path):
+    #     font["name"].setName("Italic", 2, 3, 1, 1033)
     #     font["name"].setName("UCT;JosefinSans-Italic", 3, 3, 1, 1033)
     #     font["name"].setName("Josefin Sans Italic", 4, 3, 1, 1033)
     #     font["name"].setName("JosefinSans-Italic", 6, 3, 1, 1033)
-    #     font["name"].setName("Italic", 17, 3, 1, 1033)
     # else:
+    #     font["name"].setName("Regular", 2, 3, 1, 1033)
     #     font["name"].setName("UCT;JosefinSans-Regular", 3, 3, 1, 1033)
     #     font["name"].setName("Josefin Sans Regular", 4, 3, 1, 1033)
     #     font["name"].setName("JosefinSans-Regular", 6, 3, 1, 1033)
-    #     font["name"].setName("Regular", 17, 3, 1, 1033)
 
     font.save(path + ".fix")
